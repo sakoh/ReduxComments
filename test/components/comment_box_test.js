ad0e9a50aedc9fcbf0537bdc64c164a50228b6c9
sakoh <sakohartounian@yahoo.com>
2016-06-17 15:16:10 -0700

    body added to the comment

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -24,4 +24,22 @@ describe('CommentBox' , () => {
     expect(component.find('button')).to.exist;
   });
 
+  describe('entering text', () => {
+    let textarea;
+
+    beforeEach(() => {
+      textarea = component.find('textarea');
+      textarea.simulate('change','new comment');
+    });
+
+    it('shows text entered in the textrea',() => {
+      expect(textarea).to.have.value('new comment');
+    });
+
+    it('clears the textarea when comment is submitted', () => {
+
+    });
+
+  });
+
 });
