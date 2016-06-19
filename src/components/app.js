a67a8167ced0affae277e80d0e9ebd48177dfbea
sakoh <sakohartounian@yahoo.com>
2016-06-18 23:39:17 -0700

    unnecessary props removed from App

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ export default class App extends Component {
     return (
       <div>
         <CommentBox />
-        <CommentsList comments={["nefd", "asadas"]} />
+        <CommentsList />
       </div>
     );
   }
